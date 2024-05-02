42d6baa09822a11a54978a68f2cd6dfbf775b754
Jeongyeon Kim <wjddus9132@gmail.com>
2024-05-02 23:08:47 +0900

    [feat] I Am Poor App

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by 김정연 on 5/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
