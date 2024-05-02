e5510659760bb303349b5467826090ef172dd9c0
Jeongyeon Kim <wjddus9132@gmail.com>
2024-05-02 09:31:01 +0900

    [feat] I Am Rich App

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 김정연 on 5/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
